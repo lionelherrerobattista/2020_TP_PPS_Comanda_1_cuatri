80cceac8a8d9212a469ae30504ecc3b3aaf4b55b
lionelherrerobattista <lionherrero@gmail.com>
2020-05-23 14:22:13 -0300

    Config de firebase

--- coronaDelyApp/src/environments/environment.prod.ts
+++ coronaDelyApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBWg7dk1jZY2K4EvTDTzg2GEQV7Ahjp76g",
+    authDomain: "coronadelyapp.firebaseapp.com",
+    databaseURL: "https://coronadelyapp.firebaseio.com",
+    projectId: "coronadelyapp",
+    storageBucket: "coronadelyapp.appspot.com",
+    messagingSenderId: "453398416579",
+    appId: "1:453398416579:web:19ca83a4bf7ebf79622676",
+    measurementId: "G-F9Z541XSNE"
+  },
 };
--- coronaDelyApp/src/environments/environment.ts
+++ coronaDelyApp/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBWg7dk1jZY2K4EvTDTzg2GEQV7Ahjp76g",
+    authDomain: "coronadelyapp.firebaseapp.com",
+    databaseURL: "https://coronadelyapp.firebaseio.com",
+    projectId: "coronadelyapp",
+    storageBucket: "coronadelyapp.appspot.com",
+    messagingSenderId: "453398416579",
+    appId: "1:453398416579:web:19ca83a4bf7ebf79622676",
+    measurementId: "G-F9Z541XSNE"
+  },
+
 };
 
 /*
