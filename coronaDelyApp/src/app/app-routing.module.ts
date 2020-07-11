b91d261c9cdbd6d1c19723f507e45ad9b643f63b
Paola <torrealbapa@hotmail.com.ar>
2020-07-10 22:42:51 -0300

    resuelvo merge

--- coronaDelyApp/src/app/app-routing.module.ts
+++ coronaDelyApp/src/app/app-routing.module.ts
@@ -8,20 +8,14 @@ const routes: Routes = [
   { path: 'home/:usuarioAnonimo', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'registro', loadChildren: './pages/registro/registro.module#RegistroPageModule' },
- // accesos admin  
   { path: 'registro/:object', loadChildren: './pages/registro/registro.module#RegistroPageModule' },
   { path: 'listas/:object', loadChildren: './pages/listas/listas.module#ListasPageModule' },
   { path: 'reservar', loadChildren: './pages/reserva/reserva.module#ReservaPageModule' },
-  // { path: 'reserva', loadChildren: './pages/reserva/reserva.module#ReservaPageModule' },
   { path: 'pedido', loadChildren: './pages/pedido/pedido.module#PedidoPageModule' },
-
   { path: 'inicio', loadChildren: './pages/home/home.module#HomePageModule' },
-  // { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' }
- 
-  // accesos cliente
   { path: 'delivery', loadChildren: './pages/delivery/delivery.module#DeliveryPageModule' },
   { path: 'juego', loadChildren: './pages/juego/juego.module#JuegoPageModule' },
-  { path: 'encuesta/:object', loadChildren: './pages/encuesta/encuesta.module#EncuestaPageModule'},
+  { path: 'encuesta/:idUsuario', loadChildren: './pages/encuesta/encuesta.module#EncuestaPageModule'},
   { path: 'pedido/:idUsuario', loadChildren: './pages/pedido/pedido.module#PedidoPageModule' },
   { path: 'modal-menu-detalle', loadChildren: './pages/modal-menu-detalle/modal-menu-detalle.module#ModalMenuDetallePageModule' },
   { path: 'consulta/:object', loadChildren: './pages/consulta/consulta.module#ConsultaPageModule'},
